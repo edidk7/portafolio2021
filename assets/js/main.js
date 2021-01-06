90728278549220afbffcddd7b8de14c343172bbe
edidk7 <d.ignacio292@gmail.com>
2021-01-06 17:30:46 -0300

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -193,38 +193,12 @@
         items: 3
       },
       600: {
-        items: 3
+        items: 4
       },
       1000: {
         items: 4
       }
     }
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
-      900: {
-        items: 3,
-      },
-    },
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1,
   });
 
   // Init AOS
